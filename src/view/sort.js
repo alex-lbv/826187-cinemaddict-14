d876821fb6178c917a6926b3acc391d551547053
alex-lbv <aal940722@gmail.com>
2021-04-22 17:34:19 +0300

    Поправил шаблон сортировки

--- src/view/sort.js
+++ src/view/sort.js
@@ -4,9 +4,9 @@ export default class Sort extends AbstractView {
   getTemplate() {
     return (
       `<ul class="sort">
-        <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-        <li><a href="#" class="sort__button">Sort by date</a></li>
-        <li><a href="#" class="sort__button">Sort by rating</a></li>
+        <li><a href="#" class="sort__button sort__button--active" data-sort-type="${SortType.DEFAULT}">Sort by default</a></li>
+        <li><a href="#" class="sort__button" data-sort-type="${SortType.DATE}">Sort by date</a></li>
+        <li><a href="#" class="sort__button" data-sort-type="${SortType.RATING}">Sort by rating</a></li>
       </ul>`
     );
   }
